fdd5e8d82c70a45c61ed95bfc01013d88626eb37
Ranjit Biswal <ranjitkumar.it@gmail.com>
2018-03-05 17:52:54 +0530

    commited

--- src/test/java/com/smartusys/apitest/JsonReader.java
+++ src/test/java/com/smartusys/apitest/JsonReader.java
@@ -1,5 +1,22 @@
 package com.smartusys.apitest;
 
-public class Tested {
+import java.io.FileReader;
+
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class JsonReader {
+	public static void main(String[] args) throws Exception 
+    {
+        // parsing file "JSONExample.json"
+        Object obj = new JSONParser().parse(new FileReader("E:\\workspace\\ApiTesting\\src\\test\\java\\data\\data.json"));
+         
+        // typecasting obj to JSONObject
+        JSONObject jo = (JSONObject) obj;
+         
+        // getting firstName and lastName
+        String firstName = (String) jo.get("firstName");
+		System.out.println(firstName);
+	}
 
 }
